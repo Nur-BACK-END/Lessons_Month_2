364d2cac684a36faf20b50046efdc37eed5b80e8
Nur_bek <nurelarynov46@gmail.com>
2024-11-15 21:34:21 +0600

    Feat: HW-4 emoji implementation

---
+++ HW/HW-4/HW-4.py
@@ -0,0 +1,7 @@
+import emoji
+
+message_on_WhatsApp = emoji.emojize("Привет!, Как дела? :raised_hand: ::thinking_face:")
+print(message_on_WhatsApp)
+
+# Этот код использует библиотеку emoji для добавления смайликов в текстовые сообщения.
+# Функция emojize преобразует текстовые коды эмодзи в смайлики.
