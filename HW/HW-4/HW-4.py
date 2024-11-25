14c4bf5c72d2083d265eb37dbf23b6e2d98ebd34
Nur_bek <nurelarynov46@gmail.com>
2024-11-25 23:27:27 +0600

    ДЗ-7: вывод пользователя по номеру

--- HW/HW-4/HW-4.py
+++ HW/HW-4/HW-4.py
@@ -1,6 +1,6 @@
 import emoji
 
-message_on_WhatsApp = emoji.emojize("Привет!, Как дела? :raised_hand: ::thinking_face:")
+message_on_WhatsApp = emoji.emojize("Привет!, Как дела? :raised_hand: :thinking_face:")
 print(message_on_WhatsApp)
 
 # Этот код использует библиотеку emoji для добавления смайликов в текстовые сообщения.
